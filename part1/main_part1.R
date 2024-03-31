747950d726e9055b75dfe1a209c8274bb8c6f1f9
Roman-24 <xbitarovsky@stuba.sk>
2024-03-31 15:12:53 +0200

    refaktor

--- part1/main_part1.R
+++ part1/main_part1.R
@@ -4,16 +4,16 @@ setwd(getwd())
 labor_measurements <- read.csv('./data/measurements.csv', sep='\t')
 labor_stations <- read.csv('./data/stations.csv', sep='\t')
 
-print('Zobrazenie prvých riadkov datasetov, ich rozmerov a typov atribútov \n\n')
+cat('Zobrazenie prvých riadkov datasetov, ich rozmerov a typov atribútov \n\n')
 
-print('Merenia: \n')
+cat('Merenia: \n')
 head(labor_measurements)
 cat('Number of records: ', nrow(labor_measurements), '\n')
 cat('Number of attributes: ', ncol(labor_measurements), '\n\n')
 cat('Types of attributes:\n')
 str(labor_measurements)
 
-print('Stanice: \n')
+cat('Stanice: \n')
 head(labor_stations)
 cat('Number of records: ', nrow(labor_stations), '\n')
 cat('Number of attributes: ', ncol(labor_stations), '\n\n')
@@ -21,7 +21,7 @@ cat('Types of attributes:\n')
 str(labor_stations)
 
 
-print('Zjednotenie atribútov:')
+cat('Zjednotenie atribútov:')
 # Data frame labor_measurements obsahuje iba číselné údaje float takže problém rozdielnych formátov nie je.
 # Ked sa pozrieme na labor_stations tam už to tak nieje preto si skontrolujeme formáty
 
@@ -43,7 +43,14 @@ revision_count
 # Nahradenie špecifických hodnôt
 source("part1/utils.R")
 labor_stations$revision <- sapply(labor_stations$revision, parse_dates)
-print('Identifikované rozdielne formáty dátumu sme zjednotili na formát dd mmm yyyy')
+cat('Identifikované rozdielne formáty dátumu sme zjednotili na formát dd mmm yyyy')
 # Opätovná kontrola jedinečných hodnôt a ich počtov po nahradení
 revision_count <- table(labor_stations$revision)
 revision_count
+
+cat('Nahradenie chýbajúcich hodnôt', '\n')
+missing_data_percentage <- sum(is.na(labor_measurements)) / nrow(labor_measurements) * 100
+cat(sprintf("Chýbajúce dáta v labor_measurements tvoria %.5f%% dát", missing_data_percentage))
+cat("\n")
+missing_data_percentage_stations <- sum(is.na(labor_stations)) / nrow(labor_stations) * 100
+cat(sprintf("Chýbajúce dáta v labor_stations tvoria %.5f%% dát", missing_data_percentage_stations))
