0d3d60e96eaa4fe9a97efe13c9b29d54baf96f94
Roman-24 <xbitarovsky@stuba.sk>
2024-04-03 10:05:15 +0200

    data expl

--- part1/main_part1.R
+++ part1/main_part1.R
@@ -1,13 +1,22 @@
-
 # RomanOFF read dir
 getwd()
-setwd('/Users/roman.osadsky/Desktop/ROMAN/FIIT/OZNAL_PROJECT/oznal_project1')
-#setwd(getwd())
+#setwd('/Users/roman.osadsky/Desktop/ROMAN/FIIT/OZNAL_PROJECT/oznal_project1')
+setwd(getwd())
+
+library(dplyr)
+library(ggplot2)
+library(GGally)
+library(tidyverse)
+library(caret)
+library(tidyr)
+library(e1071)
+library(corrplot)
+
 
 
 # načítanie datasetu
-labor_measurements <- read.csv('./data/measurements.csv', sep='\t')
-labor_stations <- read.csv('./data/stations.csv', sep='\t')
+labor_measurements <- read.csv('./data/measurements.csv', sep = '\t')
+labor_stations <- read.csv('./data/stations.csv', sep = '\t')
 
 cat('Zobrazenie prvých riadkov datasetov, ich rozmerov a typov atribútov \n\n')
 
@@ -40,18 +49,18 @@ labor_stations$QoS[labor_stations$QoS == "acceptable"] <- "accep"
 labor_stations$QoS[labor_stations$QoS == "maitennce"] <- "maintenance"
 # Opätovná kontrola jedinečných hodnôt a ich počtov po nahradení
 qos_count <- table(labor_stations$QoS)
-qos_count
+#qos_count
 
 # Kontrola jedinečných hodnôt a ich počtu
 revision_count <- table(labor_stations$revision)
-revision_count
+#revision_count
 # Nahradenie špecifických hodnôt
 source("part1/utils.R")
 labor_stations$revision <- sapply(labor_stations$revision, parse_dates)
 cat('Identifikované rozdielne formáty dátumu sme zjednotili na formát dd mmm yyyy')
 # Opätovná kontrola jedinečných hodnôt a ich počtov po nahradení
 revision_count <- table(labor_stations$revision)
-revision_count
+#revision_count
 
 cat('Nahradenie chýbajúcich hodnôt', '\n')
 
@@ -111,13 +120,81 @@ df <- df[, !names(df) %in% c('latitude', 'longitude')]
 df <- df[, c('location', 'warning', 'QoS', 'revision', 'TEMP', 'PRES', 'PM2.5', 'NOx', 'PM10', 'C2H3NO5', 'CH4', 'Pb', 'NH3', 'SO2', 'O3', 'CO', 'PAHs', 'H2CO', 'CFCs')]
 head(df)
 
+############## Data Exploration ##############
+
+# Spočítame počet varovaní a vypočítajte percento
+war_count <- df %>%
+  count(warning) %>%
+  rename(Warning = warning, Count = n) %>%
+  mutate(Percentage = Count / sum(Count) * 100)
+
+# Vykreslenie počtu varovaní ako koláčový graf s percentami
+ggplot(war_count, aes(x = "", y = Percentage, fill = factor(Warning))) +
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y") +
+  theme_void() +
+  labs(title = "Warning visual ratio", fill = "Warning") +
+  geom_text(aes(label = paste0(round(Percentage, 1), "%")), position = position_stack(vjust = 0.5)) +
+  scale_fill_discrete(breaks = c("0", "1"))
+
+# Vytvorenie funkcie na vykreslenie histogramov pre každý stĺpec
+plot_histogram <- function(column_name, data) {
+  ggplot(data, aes(x = !!sym(column_name))) +
+    geom_histogram(bins = 50, fill = "blue", color = "black") +
+    labs(title = paste("Histogram of", column_name), x = column_name, y = "Frequency") +
+    theme_minimal()
+}
+
+# Vygenerovanie zoznamu grafov histogramov pre každý stĺpec v dátovom rámci
+histogram_plots <- lapply(colnames(df), function(col) plot_histogram(col, df))
+
+# Výpočet skewness pre každý stĺpec v dátovom rámci 'df'
+# Identifikácia riadkov s nečíselnými hodnotami v údajne číselnom stĺpci
+df_nums_only <- df %>%
+  select(-location, -QoS, -revision) %>%
+  select(where(is.numeric))
+
+# This loop will check each numeric column for any values that cannot be converted to numeric
+for (col in names(df_nums_only)) {
+  # Convert factors to characters before testing for numeric values
+  if (is.factor(df_nums_only[[col]])) {
+    df_nums_only[[col]] <- as.character(df_nums_only[[col]])
+  }
+
+  # Test if the column can be converted to numeric and coerce non-convertible values to NA
+  if (!all(sapply(df_nums_only[[col]], is.numeric))) {
+    warning(paste("Non-numeric values found in column:", col))
+    # Coerce to numeric, warning will show non-numeric values coerced to NA
+    df_nums_only[[col]] <- as.numeric(as.character(df_nums_only[[col]]))
+  }
+}
+sapply(df_nums_only, skewness)
+
+# korelačná matica
+df_nums_only_corr <- cor(df_nums_only, use = "complete.obs")  # 'use' parameter handles missing values
+
+# Vytvorenie masky pre hodnoty, ktoré spĺňajú kritériá
+# vyfiltrovanie korelácií s abs hodnotou väčšou ako 0.39
+threshold_high <- 0.39
+threshold_low <- -0.39
+mask <- abs(df_nums_only_corr) >= threshold_high & df_nums_only_corr != 1
+
+# Copy the original correlation matrix
+filtered_corr_matrix <- df_nums_only_corr
+
+# Apply the mask: Replace values that do NOT meet the criteria with NA
+filtered_corr_matrix[!mask] <- NA
+
+# Now, plot the filtered correlation matrix
+corrplot(filtered_corr_matrix, method = "color", type = "upper", tl.col = "black", tl.srt = 45, title = "Filtered Correlation Matrix (|r| >= 0.39)")
+
+
+# Významnejšie korelácie sa javia byť medzi týmito atribútmi:
+# PM2.5 - PM10
+# PM10 - NH3
+# PM10 - C2H3NO5
 
 ############## Linear regression ##############
-## Data Exploration
-library(ggplot2)
-library(GGally)
-library(tidyverse)
-library(caret)
 theme_set(theme_bw())
 
 numeric_df <- df[sapply(df, is.numeric)]
@@ -127,19 +204,19 @@ ggpairs(numeric_df,
         diag = list(continuous = "barDiag"))
 
 ## Rozdelenie
-sample <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.7,0.3))
-train <- df[sample, ]
-test <- df[!sample, ]
+sample <- sample(c(TRUE, FALSE), nrow(df), replace = TRUE, prob = c(0.7, 0.3))
+train <- df[sample,]
+test <- df[!sample,]
 
 
-Model <-train(TEMP ~ PM2.5 + CO, data = train,
-              method = 'lm',
-              na.action = na.omit,
-              preProcess=c("scale", "center"),
-              trControl=trainControl(method = "none")
-              )
+Model <- train(TEMP ~ PM2.5 + CO, data = train,
+               method = 'lm',
+               na.action = na.omit,
+               preProcess = c("scale", "center"),
+               trControl = trainControl(method = "none")
+)
 
-Model.training <- predict(Model,train)
+Model.training <- predict(Model, train)
 Model.testing <- predict(Model, test)
 
 plot(train$PM2.5, Model.training, col = "blue")
@@ -152,3 +229,5 @@ summary(Model)
 R.train <- cor(train$PM2.5, Model.training)
 R.test <- cor(test$PM2.5, Model.testing)
 
+
+# klasifikacia
